d9b7498f76d7f29e611126481e2468b856ecd377
Simon Fish <sfish@ukcloud.com>
2018-09-09 11:10:37 +0100

    Restore full text when top of page is reached
    Fixes #29

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,38 @@ $(document).ready(function() {
   var elements = $('.sticky-top')
   elements
     .stick_in_parent()
-    .on('sticky_kit:stick', function(e) {
-      console.log('has stuck!', e.target)
-      $(e.target)
-        .find('.navbar-brand')
-        .children()
-        .addClass('compact')
-    })
-    .on('sticky_kit:unstick', function(e) {
-      console.log(e.target)
-      $(e.target)
-        .find('.navbar-brand')
-        .children()
-        .removeClass('compact')
-    })
+  //   .on('sticky_kit:stick', function(e) {
+  //     console.log('has stuck!', e.target)
+  //     $(e.target)
+  //       .find('.navbar-brand')
+  //       .children()
+  //       .addClass('compact')
+  //   })
+  //   .on('sticky_kit:unstick', function(e) {
+  //     console.log('has unstuck!', e.target)
+  //     $(e.target)
+  //       .find('.navbar-brand')
+  //       .children()
+  //       .removeClass('compact')
+  //   })
+
+   window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+      if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+          $(".sticky-top")
+            .find('.navbar-brand')
+            .find('span')
+            .children()
+            .addClass('brand-compact')
+      } else {
+          $(".sticky-top")
+            .find('.navbar-brand')
+            .find('span')
+            .children()
+            .removeClass('brand-compact')
+      }
+  }
 
   cheet('↑ ↑ ↓ ↓ ← → ← → b a', function() {
     $('main').append(
